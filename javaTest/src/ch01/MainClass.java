f9eb75e4d163eb795a0c744def740397ad516058
minju0809 <loverminju88@naver.com>
2023-10-18 20:46:06 +0900

    VO 파일에 값 넣고 받아오기

--- javaTest/src/ch01/MainClass.java
+++ javaTest/src/ch01/MainClass.java
@@ -1,47 +1,38 @@
 package ch01;
 
+
 class Test {
-	void school1(SchoolVO k) {
-		System.out.println(k);
-		System.out.println(k.getName());
-		System.out.println(k.getAge());
-		System.out.println(k.getEmail());
-	}
-
-	String school2(SchoolVO k) {
-		return k.getName() + "님";
-	}
-
-	public SchoolVO school3(SchoolVO k) {
-		k.setName(k.getName() + "냥");
-		k.setAge(k.getAge() + 100);
-		k.setEmail(k.getEmail() + "@naver.com");
-		return k;
+	public ScoreVO score(ScoreVO vo) {
+		String str = "";
+		String grade = vo.getGrade();
+		int score = vo.getScore();
+		if (score >= 90) {
+			grade = "A";
+			str = "점수 : " + score + "점, 등급 : " + grade;
+		} else if (score >= 80) {
+			grade = "B";
+			str = "점수 : " + score + "점, 등급 : " + grade;
+		} else if (score >= 70){
+			grade = "C";
+			str = "점수 : " + score + "점, 등급 : " + grade;
+		} else {
+			grade = "F";
+			str = "점수 : " + score + "점, 등급 : " + grade + ", 재시험";
+		}
+		vo.setStr(str);
+		return vo;
 	}
 }
 
 public class MainClass {
 	public static void main(String[] args) {
 		Test test = new Test();
-
-		String name = "둘리";
-		int age = 30;
-		String email = "minju";
-
-		SchoolVO vo = new SchoolVO();
-		vo.setName(name);
-		vo.setAge(age);
-		vo.setEmail(email);
-
-		test.school1(vo);
-
-		String result = test.school2(vo);
 		
-		/////////////////////////
-		System.out.println(result);
-
-		/////////////////////////
-		vo = test.school3(vo);
-		System.out.println(vo.getName() + vo.getAge() + vo.getEmail());
+		int score = 60;		
+		ScoreVO vo = new ScoreVO();
+		vo.setScore(score);
+		vo = test.score(vo);
+		
+		System.out.println(vo.getStr());
 	}
 }
---
+++ javaTest/src/ch01/ScoreVO.java
@@ -0,0 +1,31 @@
+package ch01;
+
+public class ScoreVO {
+	int score;
+	String grade;
+	String str;
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+}
