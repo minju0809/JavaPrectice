bc942e0c513c23fb1ae04f18b4af9d8ec4ca42cf
minju0809 <loverminju88@naver.com>
2023-10-18 21:58:49 +0900

    GugudanVO 생성하여 ArrayList로 구구단 출력

---
+++ javaTest/src/ch01/GugudanVO.java
@@ -0,0 +1,32 @@
+package ch01;
+
+public class GugudanVO {
+	int startDan;
+	int endDan;
+	String str;
+
+	public int getStartDan() {
+		return startDan;
+	}
+
+	public void setStartDan(int startDan) {
+		this.startDan = startDan;
+	}
+
+	public int getEndDan() {
+		return endDan;
+	}
+
+	public void setEndDan(int endDan) {
+		this.endDan = endDan;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+}
--- javaTest/src/ch01/MainClass.java
+++ javaTest/src/ch01/MainClass.java
@@ -1,38 +1,49 @@
 package ch01;
 
+import java.util.ArrayList;
+import java.util.List;
 
 class Test {
-	public ScoreVO score(ScoreVO vo) {
-		String str = "";
-		String grade = vo.getGrade();
-		int score = vo.getScore();
-		if (score >= 90) {
-			grade = "A";
-			str = "점수 : " + score + "점, 등급 : " + grade;
-		} else if (score >= 80) {
-			grade = "B";
-			str = "점수 : " + score + "점, 등급 : " + grade;
-		} else if (score >= 70){
-			grade = "C";
-			str = "점수 : " + score + "점, 등급 : " + grade;
-		} else {
-			grade = "F";
-			str = "점수 : " + score + "점, 등급 : " + grade + ", 재시험";
+	public List<GugudanVO> gugudan(GugudanVO vo) {
+		int startDan = vo.getStartDan();
+		int endDan = vo.getEndDan();
+		List<GugudanVO> li = new ArrayList<GugudanVO>();
+		for (int dan = startDan; dan <= endDan ; dan++) {
+			int i = 1;
+			String str = "";
+			while(i < 10) {
+				if(dan*i < 10) {
+					str = dan + "X" + i + "=" + " " +(dan*i) + " ";
+				} else {
+					str = dan + "X" + i + "=" + (dan*i) + " ";
+				}
+				vo = new GugudanVO();
+				vo.setStr(str);
+				li.add(vo);
+				i++;
+			}
 		}
-		vo.setStr(str);
-		return vo;
+		return li;
 	}
 }
 
 public class MainClass {
 	public static void main(String[] args) {
+		
 		Test test = new Test();
 		
-		int score = 60;		
-		ScoreVO vo = new ScoreVO();
-		vo.setScore(score);
-		vo = test.score(vo);
+		GugudanVO vo = new GugudanVO();
+		vo.setStartDan(2);
+		vo.setEndDan(8);
 		
-		System.out.println(vo.getStr());
+		List<GugudanVO> m = test.gugudan(vo);
+		for(int i = 0; i < m.size(); i++) {
+			GugudanVO result = m.get(i);
+			System.out.print(result.getStr());
+			
+			if((i+1) % 9 == 0) {
+				System.out.println();
+			}
+		}
 	}
 }
