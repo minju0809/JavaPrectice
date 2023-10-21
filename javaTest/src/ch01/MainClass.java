389feb871cdc0adb7f61d8097ed54871885bd612
minju0809 <loverminju88@naver.com>
2023-10-21 22:07:37 +0900

    VO 활용 연습, 강제형변환까지

--- javaTest/src/ch01/MainClass.java
+++ javaTest/src/ch01/MainClass.java
@@ -41,11 +41,67 @@ class Test {
 		scanner.close();
 		System.out.println("프로그램 종료");
 	}
+
+	public SchoolVO school(SchoolVO vo) {
+		vo.setName(vo.getName() + "님의 나이는 ");
+		vo.setAge(vo.getAge() + 10);
+		vo.setEmail(", 이메일은 " + vo.getEmail() + "@naver.com");
+		return vo;
+	}
+
+	public NumberVO sum(NumberVO vo) {
+		int num11 = Integer.parseInt(vo.getNum1());
+		int num22 = Integer.parseInt(vo.getNum2());
+		int sum = num11 + num22;
+		vo.setResult(sum + "");
+		return vo;
+	}
 }
 
 public class MainClass {
 	public static void main(String[] args) {
-		Test test = new Test();
-		test.accountBook();
+//		Test test = new Test();
+
+		// accountBook
+//		test.accountBook();
+
+		// school
+//		String name = "둘리";
+//		int age = 15;
+//		String email = "minju";
+//
+//		SchoolVO vo = new SchoolVO();
+//
+//		vo.setName(name);
+//		vo.setAge(age);
+//		vo.setEmail(email);
+//
+//		vo = test.school(vo);
+//		System.out.println(vo.getName() + vo.getAge() + vo.getEmail());
+
+//		String num1 = "10";
+//		String num2 = "20";
+//		
+//		NumberVO vo = new NumberVO();
+//		
+//		vo.setNum1(num1);
+//		vo.setNum2(num2);
+//		
+//		NumberVO result = test.sum(vo);
+//		System.out.println(result.getResult());
+
+		// String -> int
+		String num1 = "100";
+		int i = Integer.parseInt(num1);
+		System.out.println(i);
+
+		// int -> String
+		int num2 = 20;
+		int num3 = 40;
+		String str1 = String.valueOf(num2);
+		String str2 = String.valueOf(num3);
+		System.out.println("int의 합: " + (num2 + num3));
+		System.out.println("String을 추가하여 변환: " + num2 + num3);
+		System.out.println("강제 형변환: " + str1 + str2);
 	}
 }
---
+++ javaTest/src/ch01/NumberVO.java
@@ -0,0 +1,31 @@
+package ch01;
+
+public class NumberVO {
+	String num1;
+	String num2;
+	String result;
+
+	public String getNum1() {
+		return num1;
+	}
+
+	public void setNum1(String num1) {
+		this.num1 = num1;
+	}
+
+	public String getNum2() {
+		return num2;
+	}
+
+	public void setNum2(String num2) {
+		this.num2 = num2;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+}
